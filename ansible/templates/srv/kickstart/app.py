0673aeb8ae671a34705dc6ac5a2ea4a5f63acba5
Schlueter <bs@schlueter.blue>
2016-10-08 00:07:41 -0400

    Make kickstart app get template target via querystring

--- ansible/templates/srv/kickstart/app.py
+++ ansible/templates/srv/kickstart/app.py
@@ -1,9 +1,10 @@
+# pylint: disable=broad-except
 import os
 import sys
-
-from jinja2 import Template
 from pprint import pformat
+
 import yaml
+from jinja2 import Template
 
 APP_DIR = os.path.dirname(__file__)
 HOST_VARS = os.path.join(APP_DIR, 'host_vars.yml')
@@ -22,12 +23,20 @@ def get_requestor_vars(requestor_ip):
         raise HostVarsNotFound(requestor_ip)
     return host_vars
 
-def get_named_template(name):
-    template_path = TEMPLATE_ROOT + name
+def find_template_file(path, name):
     try:
-        template = open(template_path).read()
-    except IOError as e:
-        raise TemplateNotFound(template_path)
+        return os.path.join(path, [f for f in os.listdir(path) if f.startswith(name)][0])
+    except:
+        raise TemplateNotFound("find_template_file({}, {})".format(path, name))
+
+def get_named_template(name):
+    template_path = os.path.join(TEMPLATE_ROOT, os.path.dirname(name))
+    template_name = os.path.basename(name)
+    template_file = find_template_file(template_path, template_name)
+    try:
+        template = open(template_file).read()
+    except:
+        raise TemplateNotFound(template_file)
     return template
 
 def kickstart_template(template_name=None, requestor_ip=None):
@@ -39,19 +48,19 @@ def application(env, start_response):
     try:
         response = '# Generated kickstart file for {} from {}\n\n{}\n'.format(
             env['REMOTE_ADDR'],
-            env['REQUEST_URI'],
-            kickstart_template(template_name=env['REQUEST_URI'], requestor_ip=env['REMOTE_ADDR'])
+            env['QUERY_STRING'],
+            kickstart_template(template_name=env['QUERY_STRING'], requestor_ip=env['REMOTE_ADDR'])
         )
-        
-        start_response('200 OK', [('Content-Type','text/html')])
-    except Exception as e:
-        start_response('404 ', [('Content-Type','text/html')])
-        response = '{}\n'.format(pformat(e))
+
+        start_response('200 OK', [('Content-Type', 'text/html')])
+    except Exception as err:
+        start_response('404 ', [('Content-Type', 'text/html')])
+        response = '{}\n'.format(pformat(err))
 
     return [response]
 
 if __name__ == '__main__':
-    def start_response(text, headers): 
+    def start_response_mock(text, headers):
         print text
         print headers
-    print  '\n{}'.format(application(yaml.load(sys.argv[1]), start_response))
+    print  '\n{}'.format(application(yaml.load(sys.argv[1]), start_response_mock))
